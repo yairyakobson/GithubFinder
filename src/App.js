89b1a393ff789a0077ef22d076c7bb687f4b0134
yair2000 <32851854+yair2000@users.noreply.github.com>
2022-02-15 16:20:28 +0200

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App(){
+  return(
+    <div className="bg-purple-500">
+      <h1 className='text-xl'>Hello World</h1>
+      <button class="btn">Click</button>
+    </div>
+  );
+}
+export default App;
