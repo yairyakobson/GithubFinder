ded0ffeafdf13d3bff3639bc2a25ed0ca5b0bcd8
yair2000 <32851854+yair2000@users.noreply.github.com>
2022-02-18 09:29:25 +0200

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import {BrowserRouter as Router, Route, Routes} from "react-router-dom"
 import Navbar from "./components/layout/Navbar";
 import Footer from "./components/layout/Footer";
+import Alert from "./components/layout/Alert";
 import About from "./pages/About";
 import Error from "./pages/Error";
 import Home from "./pages/Home";
 import { GitProvider } from "./context/github/GitContext";
+import { AlertProvider } from "./context/alert/AlertContext";
 
 function App(){
   return(
     <GitProvider>
+      <AlertProvider>
       <Router className="bg-purple-500">
         <div className="flex flex-col justify-between h-screen">
           <Navbar/>
             <main className="container mx-auto px-3 pb-12">
+              <Alert/>
               <Routes>
                 <Route path="/" element={<Home/>}/>
                 <Route path="/About" element={<About/>}/>
@@ -23,6 +27,7 @@ function App(){
           <Footer/>
         </div>
       </Router>
+    </AlertProvider>
   </GitProvider>
   );
 }
