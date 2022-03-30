8764afe6b8779535458ce4f475b4b8df6716aa03
yair2000 <32851854+yair2000@users.noreply.github.com>
2022-03-30 15:58:19 +0300

    Changed to the latest API
    React 18 new API

--- src/index.js
+++ src/index.js
@@ -1,11 +1,8 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import * as ReactDOMClient from 'react-dom/client';
 import App from './App';
 import './index.css'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const container = document.getElementById('root');
+const root = ReactDOMClient.createRoot(container);
+root.render(<App/>);
