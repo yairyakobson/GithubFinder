9166a5feb32baffd61ef6474b5c900ac3d0d733d
yair2000 <32851854+yair2000@users.noreply.github.com>
2022-04-17 17:45:39 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,5 @@ import * as ReactDOMClient from 'react-dom/client';
 import App from './App';
 import './index.css'
 
-const container = document.getElementById('root');
-const root = ReactDOMClient.createRoot(container);
+const root = ReactDOMClient.createRoot(document.getElementById('root'));
 root.render(<App/>);
