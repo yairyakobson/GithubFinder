d1e65815d07c8cfedc77638b3d1c7454bca3712f
yair2000 <32851854+yair2000@users.noreply.github.com>
2022-04-17 18:00:18 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import React from 'react';
-import * as ReactDOMClient from 'react-dom/client';
+import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css'
 
-const root = ReactDOMClient.createRoot(document.getElementById('root'));
-root.render(<App/>);
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+<React.StrictMode>
+    <App/>
+</React.StrictMode>
+);
