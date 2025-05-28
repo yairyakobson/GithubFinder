3dc2815af25a3d6f48bc33e478c8c27b203b9099
yairyakobson <yairnet92@gmail.com>
2025-05-28 17:22:02 +0300

    modified: refactored homepage

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,17 +1,10 @@
-import reactLogo from "../components/layout/assets/react.svg";
+import UsersSearch from "../components/users/UsersSearch";
 
 function Home(){
   return(
     <>
-      <section className="card bg-base-100 w-96 shadow-sm">
-        <figure>
-          <img src={reactLogo}
-          alt="React Logo"/>
-        </figure>
-        <section className="card-body">
-          <h2 className="card-title">Vite + React</h2>
-          <p>Click on the Vite and React logos to learn more</p>
-        </section>
+      <section>
+        <UsersSearch/>
       </section>
     </>
   )
